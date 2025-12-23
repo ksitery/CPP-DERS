eec61e944377e9c1aaa80c4f822858e7f4128d77
Hanım Hayat Akdağ <hanimakdag46@gmail.com>
2025-12-23 23:44:08 +0300

    Clean up unused files

--- ehliyet.py
+++ ehliyet.py
@@ -2,4 +2,4 @@ year=input("How old are you? ")
 if int(year)>=18:
     print("Ehliyet alabilirsin! 🚗")
 else:
-    print("Ehliyet alamazsın, büyü de gel.")
+    print("Ehliyet alamazsin, büyü de gel.")
